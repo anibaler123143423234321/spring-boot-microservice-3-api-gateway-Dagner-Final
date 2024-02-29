c6291e820775d59faecd3871c4bad9f7adc822a6
Dagner Chuman <93721929+anibaler123143423234321@users.noreply.github.com>
2024-02-29 12:56:16 -0500

    Mejora para negocio request en produccion

--- src/main/java/com/dagnerchuman/springbootmicroservice3ApiGateway/request/NegocioServiceRequest.java
+++ src/main/java/com/dagnerchuman/springbootmicroservice3ApiGateway/request/NegocioServiceRequest.java
@@ -8,7 +8,7 @@ import java.util.List;
 @FeignClient(
         value="negocio-service",
         //path="api/negocios",
-        url="${compras.service.url}",
+        url="${negocio.service.url}",
         configuration = FeignConfiguration.class
 )
 public interface NegocioServiceRequest {
